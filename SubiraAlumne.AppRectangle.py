e249ca0f90fe17784d9116db0b1dd51f77e38bb8
cesarsubira2001@ginebro.cat <cesarsubira2001@ginebro.cat>
2023-03-30 19:01:42 +0200

    Commit de les 19:01

---
+++ SubiraAlumne.AppRectangle.py
@@ -0,0 +1,8 @@
+from SubiraAlumneRectangle import SubiraAlumneRectangle
+
+base = int()
+alssada = int()
+
+
+base = int(input("Posa un numero como base: "))
+alssada = int(input("Posa un numero como altura: "))
---
+++ SubiraAlumneRectangle.py
@@ -0,0 +1,31 @@
+class Rectangle:
+    # constructor
+    def _ini_(self, _base, _alssada):
+        self.base = _base
+        self.alssada = _alssada
+
+    # setter
+
+    def setBase(self, _base):
+        self.base = _base
+
+    def seAlssada(self, _alssada):
+        self.alssada = _alssada
+    # getters
+
+    def getBase(self):
+        return self.base
+
+    def getAlsada(self):
+        return self.alssada
+
+    # Metodes
+
+    def calcularPerimetre(base, alssada):
+        print("El Perímetre es: " + str(2 * (alssada + base)))
+
+    def calcularArea(base, alssada):
+        print("l'area es: " + str(alssada * base))
+
+    def __str__(self):
+        return (f"Base: {self.base}\nAlssasda: {self.alssada}")
